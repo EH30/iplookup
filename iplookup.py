258116f681f7125ea82b58d973c63bc84a645efe
EH30 <games.eh2000@gmail.com>
2020-03-28 20:18:49 +0530

    Add files via upload

---
+++ iplookup.py
@@ -0,0 +1,34 @@
+import urllib.request
+import sys
+import json
+
+def getInfo(IP):
+    url = "http://ip-api.com/json/"
+    req = urllib.request.urlopen(url + IP)
+    json_value = json.loads(req.read())
+    try:
+        print("IP: ", json_value["query"])
+        print("status: ", json_value["status"])
+        print("country: ", json_value["country"])
+        print("countryCode: ", json_value["countryCode"])
+        print("region: ", json_value["region"])
+        print("regionName: ", json_value["regionName"])
+        print("city: ", json_value["city"])
+        print("zip: ", json_value["zip"])
+        print("lat: ", json_value["lat"])
+        print("lon: ", json_value["lon"])
+        print("timezone: ", json_value["timezone"])
+        print("isp: ", json_value["isp"])
+        print("org: ", json_value["org"])
+        print("as: ", json_value["as"])
+    except KeyError:
+        pass
+
+
+if __name__ == "__main__":
+    try:
+        getInfo(sys.argv[1])
+    except IndexError:
+        print("[-]IndexError")
+        print("Usage: python iptracker_api.py IP")
+        
